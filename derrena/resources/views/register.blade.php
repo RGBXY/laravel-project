ff0e0e6093ebfc0465cf3f148d17431a7eacd01d
RGBXY <derrenamadeo@gmail.com>
2023-10-05 15:04:37 +0700

    Create Register View

---
+++ derrena/resources/views/register.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+</head>
+<body>
+        <div class="position-absolute top-50 start-50 translate-middle border border-black rounded">
+            <div class="form p-5">
+                <form>
+                    <h1>Daftar</h1>
+                    <div class="form-group mb-3">
+                      <input type="username" class="form-control" id="username" aria-describedby="username" placeholder="Username">
+                    </div>
+                    <div class="form-group mb-3">
+                        <input type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Email">
+                      </div>
+                    <div class="form-group mb-3">
+                      <input type="password" class="form-control" id="Password" placeholder="Password">
+                    </div>
+                    <div class="form-group mb-3">
+                      <input type="password" class="form-control" id="Password" placeholder="Confirm Password">
+                    </div>
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                  </form>
+            </div>
+         </div>
+        </div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+</body>
+</html>
